95eba3a9f63b0b40d3fc5b5f4873963799f15bdb
Fumihiko-Goto <69379393+Fumihiko-Goto@users.noreply.github.com>
2020-10-31 13:15:00 +0900

    Add files via upload

---
+++ gs_php3_02shukudai/19.gotou.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.3
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: localhost:8889
+-- 生成日時: 2020 年 10 月 31 日 04:13
+-- サーバのバージョン： 5.7.26
+-- PHP のバージョン: 7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- データベース: `gs_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `gs_user_table`
+--
+
+CREATE TABLE `gs_user_table` (
+  `id` int(12) NOT NULL,
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `lid` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `lpw` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `kanli_flg` int(1) NOT NULL,
+  `life_flg` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `gs_user_table`
+--
+
+INSERT INTO `gs_user_table` (`id`, `name`, `lid`, `lpw`, `kanli_flg`, `life_flg`) VALUES
+(7, '郷ひろみ', 'aghjk', 'Asdfg2', 1, 0),
+(10, 'ふくしま', 'aaaaae', 'Qwer12345', 0, 0),
+(11, 'マツダ', 'ぱすだよん', 'Gwert643222', 0, 0);
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `gs_user_table`
+--
+ALTER TABLE `gs_user_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- ダンプしたテーブルのAUTO_INCREMENT
+--
+
+--
+-- テーブルのAUTO_INCREMENT `gs_user_table`
+--
+ALTER TABLE `gs_user_table`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
